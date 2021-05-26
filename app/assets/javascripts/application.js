a4f56edfe19ad2eb7ca63956911cc245b3c6be3d
thkernel <a.dembele@upcase.net>
2021-05-26 15:53:09 +0000

    add stats on dashboard

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,20 +27,27 @@
 //= require bootstrap
 //= require toastr
 //= require rails.validations
-//= require js/bracket
-//= require lib/moment/min/moment.min
+
+// NEW BRACKET JS
+//= require lib/jquery-ui/ui/widgets/datepicker
+//= require lib/bootstrap/js/bootstrap.bundle.min
 //= require lib/perfect-scrollbar/perfect-scrollbar.min
-//= require js/ResizeSensor
-//= require js/dashboard
+//= require lib/moment/min/moment.min
+//= require lib/peity/jquery.peity.min
 //= require lib/rickshaw/vendor/d3.min
 //= require lib/rickshaw/vendor/d3.layout.min
 //= require lib/rickshaw/rickshaw.min
-//= require lib/datatables.net/js/jquery.dataTables.min
-//= require lib/datatables.net-dt/js/dataTables.dataTables.min
-//= require lib/datatables.net-responsive/js/dataTables.responsive.min
-//= require lib/datatables.net-responsive-dt/js/responsive.dataTables.min
-//= require lib/peity/jquery.peity.min
 //= require lib/jquery.flot/jquery.flot
+//= require lib/jquery.flot/jquery.flot.resize
+//= require lib/flot-spline/js/jquery.flot.spline.min
+// require lib/jquery-sparkline/jquery.sparkline.min
+// require lib/echarts/echarts.min
+//= require lib/select2/js/select2.full.min
+
+//= require js/bracket
+//= require js/ResizeSensor
+//= require js/dashboard
+
 //= require google_analytics
 // require cookies_eu
 // require jquery.uploadPreview.min
