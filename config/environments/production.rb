693bf844b8cbcbe7603bee8c7022612ac4e8d0ba
thkernel <a.dembele@upcase.net>
2021-07-26 08:59:00 +0000

    update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,6 +98,7 @@ Rails.application.configure do
 config.action_mailer.perform_deliveries = true
 config.action_mailer.raise_delivery_errors = true
 config.action_mailer.default :charset => "utf-8"
+  Rails.application.routes.default_url_options[:host] = 'https://holilink.ml'
 
 config.action_mailer.default_url_options = { protocol: "https", host: Rails.application.credentials.dig(:email, :production, :host)  }
 config.action_mailer.delivery_method = :smtp
